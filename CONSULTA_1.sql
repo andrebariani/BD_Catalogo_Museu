0f571474d7faceb44ba20deb8d7aac6d344a3e87
andrebariani <andrebariani@gmail.com>
2019-05-26 16:05:13 -0300

    initial push

---
+++ CONSULTA_1.sql
@@ -0,0 +1,10 @@
+﻿SELECT nome_mus AS Nome_do_museu, nome_col AS Nome_coleção, tipo_col AS Tipo_coleção, idioma AS Idioma, preco_ent AS Preço_entrada
+FROM colecao AS c NATURAL JOIN museu AS m ON (tipo_col LIKE 'b%'); 
+WHERE tipo_col ILIKE 'b%' AND m.cod_mus = c.cod_mus;
+
+--Consultas--
+
+--Consulta 1--
+SELECT nome_mus AS Nome_do_museu, nome_col AS Nome_coleção, tipo_col AS Tipo_coleção, idioma AS Idioma, preco_ent AS Preço_entrada
+FROM museu AS m, colecao AS c 
+WHERE tipo_col ILIKE 'História' AND m.cod_mus = c.cod_mus;
---
+++ CONSULTA_2.sql
@@ -0,0 +1,12 @@
+﻿--Consultas--
+
+--Consulta 2--
+SELECT nome_mus AS Nome_do_museu, tipo_ativ AS Tipo_atividade, preco_ativ AS Preço_atividade, nome_ativ AS Nome_atividade, cidade AS Cidade
+FROM museu AS m, atividade AS a
+WHERE m.cod_mus = a.cod_mus AND preco_ativ <= 10
+ORDER BY preco_ativ;
+
+--Índice utilizado na consulta 2-- 
+CREATE INDEX IX_preco_ativ ON atividade(preco_ativ ASC);
+
+DROP INDEX IX_preco_ativ;
